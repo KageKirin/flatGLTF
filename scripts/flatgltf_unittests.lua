b0832ec41b9a20970c0dd760f0a0f37f606cabaf
KageKirin <kagekirin@gmail.com>
2017-07-29 17:11:00 +0900

    added genie luafile for unit test projects

---
+++ scripts/flatgltf_unittests.lua
@@ -0,0 +1,37 @@
+-- premake for flatgltf_unittests
+
+---
+
+if generate_thirdparty_projects and generate_thirdparty_projects == true then
+
+---
+
+project "flatgltf_2_0_loader"
+	kind "ConsoleApp"
+	language "C++"
+	flags {}
+
+	includedirs {
+		flatgltf_includedirs,
+		flatbuffers_includedirs,
+		catch_includedirs,
+	}
+
+	defines {
+	}
+
+	files {
+		path.join(flatgltf_root, "test", "flatgltf_2_0_loader.cpp"),
+	}
+
+	links {
+		"flatgltf_2_0",
+		"flatbuffers",
+	}
+
+build_cppfwd("flatgltf_2_0_loader")
+
+end -- _OPTIONS["with-thirdparty"] ~= nil
+
+---
+
