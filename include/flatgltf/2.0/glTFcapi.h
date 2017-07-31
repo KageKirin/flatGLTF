b530fcc4065cafdca87ab02732f5ef2ad54afb41
KageKirin <kagekirin@gmail.com>
2017-07-31 13:13:34 +0900

    added (TODO) header for C-API

---
+++ include/flatgltf/2.0/glTFcapi.h
@@ -0,0 +1,31 @@
+#ifndef FLATGLTF_2_0_CAPI_H_INC
+#define FLATGLTF_2_0_CAPI_H_INC
+
+#include "flatgltf/common/glTFapiexports.h"
+
+#include <cctype>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif	// defined(__cplusplus)
+
+///-----------------------------------------------------------------------
+/// id type alias
+///-----------------------------------------------------------------------
+
+typedef int32_t glTFid_t;
+
+///-----------------------------------------------------------------------
+/// document structure (PIMPL)
+///-----------------------------------------------------------------------
+typedef struct Document glTF_2_0_Document;
+
+
+/// TODO: implement API
+
+#if defined(__cplusplus)
+}	// extern "C"
+#endif	// defined(__cplusplus)
+
+
+#endif	// ! FLATGLTF_2_0_CAPI_H_INC
